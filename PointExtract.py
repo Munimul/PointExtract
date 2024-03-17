ef5426392bce44539364f1ef54fd413c0e85f1d0
munimul hasan <munimul999@gmail.com>
2024-03-16 20:15:02 -0500

    plot window class added with button in main window

--- PointExtract.py
+++ PointExtract.py
@@ -1,15 +1,51 @@
 import sys
 import glob
 import os
-sys.dont_write_bytecode=True
-import os
+import matplotlib
+from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
 from PyQt6.QtCore import QSize,Qt,QPoint
 from PyQt6.QtWidgets import  QApplication, QWidget, QPushButton, QVBoxLayout, QToolBar, QFileDialog, QLabel, QHBoxLayout, QMainWindow, QStatusBar
 from PyQt6.QtGui import QPixmap, QPen,QPainter,QColor, QAction, QPolygon
 
-class ImageLabel(QLabel):
+
+class MplCanvas(FigureCanvas):
+
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+        super(MplCanvas, self).__init__(fig)
+
+
+class GraphWindow(QWidget):
+    def __init__(self,x,y):
+        super().__init__()
+
+        self.setWindowTitle("PointExtract_Plot")
+        self.x=x
+        self.y=y
+        Layout= QVBoxLayout()       
+        toolbar = QToolBar()
+        sc = MplCanvas(self, width=5, height=4, dpi=100)
+        sc.axes.scatter(self.x, self.y)
+        sc.axes.axis('off')
+        sc.axes.set_aspect('equal')
+        
+
+        button_rotate= QAction("Rotate", self)
+        button_rotate.setStatusTip("Rotate points")
+
+        toolbar.addAction(button_rotate)
+        Layout.addWidget(toolbar)
+        Layout.addWidget(sc)
+
+        self.setLayout(Layout)
+        self.setMinimumSize(QSize(416, 600))
+        
+
+class DrawPoint(QLabel):
     def __init__(self, parent=None):
-        super(ImageLabel, self).__init__(parent)
+        super(DrawPoint, self).__init__(parent)
         self.points = QPolygon()
         self.draw=False
 
@@ -32,9 +68,13 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
 
+
+
+
         self.setWindowTitle("PointExtract")
 
-        self.img = ImageLabel()
+        self.w=None #No window yet
+        self.img = DrawPoint()
         self.img.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         self.setCentralWidget(self.img)        
@@ -66,6 +106,10 @@ class MainWindow(QMainWindow):
         button_clear = QAction("Clear",self)
         button_clear.setStatusTip("Clear All Points")
         button_clear.triggered.connect(self.onClearButtonClick)
+
+        button_plot = QAction("Plot",self)
+        button_plot.setStatusTip("Plot All Points")
+        button_plot.triggered.connect(self.onPlotButtonClick)
         
         toolbar.addAction(button_file)
         toolbar.addAction(button_previous)
@@ -73,9 +117,10 @@ class MainWindow(QMainWindow):
         toolbar.addAction(self.button_draw)
         toolbar.addAction(button_undo)
         toolbar.addAction(button_clear)
+        toolbar.addAction(button_plot)
 
         self.setStatusBar(QStatusBar(self))
-        self.setMinimumSize(QSize(600, 600))
+        self.setMinimumSize(QSize(416, 600))
 
         self.folderDir=None
         self.imagePath=None
@@ -103,7 +148,7 @@ class MainWindow(QMainWindow):
             
 
     def imageShow(self,index):
-        pixmap=QPixmap(self.imageFiles[index]).scaledToWidth(512)
+        pixmap=QPixmap(self.imageFiles[index]).scaledToWidth(416)
         self.img.setPixmap(pixmap)
         self.img.setFixedSize(pixmap.size())
         self.img.setAlignment(Qt.AlignmentFlag.AlignCenter)
@@ -134,6 +179,20 @@ class MainWindow(QMainWindow):
         if not self.img.points.isEmpty():
             self.img.points.remove(self.img.points.size()-1)
             self.img.update()
+
+    def onPlotButtonClick(self):
+        offset=self.img.height()
+        print(self.img.height())
+        print(len(self.img.points))
+        x=[]
+        y=[]
+        if len(self.img.points)!=0:
+            for point in self.img.points:
+                x.append(point.x())
+                y.append(offset-point.y())
+        print(x,y)
+        self.w = GraphWindow(x,y)  # image points start from the top left
+        self.w.show()
 
 
     
